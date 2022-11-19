383f016aa9df21d59beb6638215f6600d52b87e5
JBB3-LBH <jbbrown@hypercho.com>
2022-11-19 17:43:13 +0000

    _id error fixed

--- src/services/Signed.ts
+++ src/services/Signed.ts
@@ -58,7 +58,11 @@ export const signedUrl_forScenes = async (ScenesList: TypesforScene[]): Promise<
   for (let i = 0; i < ScenesList.length; i++) {
     const { videoId, _id } = ScenesList[i];
     if (!videoId || !_id) {
-        finalArr.push({ videoId: "", _id });
+        if(!_id){
+            finalArr.push({ videoId: "", _id:"" });
+        }else{
+            finalArr.push({ videoId: "", _id });
+        }
       }
     else if (videoId.includes("http")) {
       finalArr.push({ videoId: "", _id });

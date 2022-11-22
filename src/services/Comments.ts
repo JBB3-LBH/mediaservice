e41e8cbe8eb413ed71ad4613fde4234479fdfd7d
JBB3-LBH <jbbrown@hypercho.com>
2022-11-22 17:19:15 +0000

    comment pic signing error fixed

--- src/services/Comments.ts
+++ src/services/Comments.ts
@@ -2,10 +2,6 @@ import { Reply, Comment } from "../models";
 import logg from "../Logs/Customlog";
 import { ObjectId } from "mongodb";
 import { ResultTypes } from "../types/main";
-import { custom } from "./custom";
-import { getSignedUrl } from "@aws-sdk/cloudfront-signer";
-import dotenv from "dotenv";
-dotenv.config();
 
 //types for the function to get all/some comments
 export interface getCommentsTypes {
@@ -41,53 +37,6 @@ if(comments.length < 1){
     code:200
 }}
 
-for(const comment of comments){
-//loop through every comment 
-//sign the images on both comment and replies.
-
-/* <---for comments --> */
-  if(comment.profilePic){
-    comment.profilePic = getSignedUrl({
-      url: `https://d27i2oedcihbcx.cloudfront.net/${comment.profilePic}`,
-      keyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID!,
-      privateKey: custom,
-      dateLessThan: `${new Date(Date.now() + 60 * 60 * 24)}`,
-    });
-  }
-  if(comment.channelPic){
-    comment.channelPic = getSignedUrl({
-      url: `https://d27i2oedcihbcx.cloudfront.net/${comment.channelPic}`,
-      keyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID!,
-      privateKey: custom,
-      dateLessThan: `${new Date(Date.now() + 60 * 60 * 24)}`,
-    });
-  }
-/* -------------------- */
-    
-/* <---for replies---> */
-//loop through the replies and sign its pics too
-    const replies = comment.replies
-    for(const reply of replies){
-      if(reply.profilePic){
-        reply.profilePic = getSignedUrl({
-          url: `https://d27i2oedcihbcx.cloudfront.net/${reply.profilePic}`,
-          keyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID!,
-          privateKey: custom,
-          dateLessThan: `${new Date(Date.now() + 60 * 60 * 24)}`,
-        });
-      }
-      if(reply.channelPic){
-        reply.channelPic = getSignedUrl({
-          url: `https://d27i2oedcihbcx.cloudfront.net/${reply.channelPic}`,
-          keyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID!,
-          privateKey: custom,
-          dateLessThan: `${new Date(Date.now() + 60 * 60 * 24)}`,
-        });
-      }
-    }
-/* <------------------> */
-
-}
       return { success: true, code: 200, data:comments };
     }
     // else, return the initial
@@ -109,53 +58,7 @@ for(const comment of comments){
           code:200
       }}
       
-      for(const comment of comments){
-      //loop through every comment 
-      //sign the images on both comment and replies.
-      
-      /* <---for comments --> */
-        if(comment.user){
-          comment.user.profilePic = getSignedUrl({
-            url: `https://d27i2oedcihbcx.cloudfront.net/${comment.user.profilePic}`,
-            keyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID!,
-            privateKey: custom,
-            dateLessThan: `${new Date(Date.now() + 60 * 60 * 24)}`,
-          });
-        }
-        if(comment.channel){
-          comment.channel.channelPic = getSignedUrl({
-            url: `https://d27i2oedcihbcx.cloudfront.net/${comment.channel.channelPic}`,
-            keyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID!,
-            privateKey: custom,
-            dateLessThan: `${new Date(Date.now() + 60 * 60 * 24)}`,
-          });
-        }
-      /* -------------------- */
-          
-      /* <---for replies---> */
-      //loop through the replies and sign its pics too
-          const replies = comment.replies
-          for(const reply of replies){
-            if(reply.user){
-              reply.user.profilePic = getSignedUrl({
-                url: `https://d27i2oedcihbcx.cloudfront.net/${reply.user.profilePic}`,
-                keyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID!,
-                privateKey: custom,
-                dateLessThan: `${new Date(Date.now() + 60 * 60 * 24)}`,
-              });
-            }
-            if(reply.channel){
-              reply.channel.channelPic = getSignedUrl({
-                url: `https://d27i2oedcihbcx.cloudfront.net/${reply.channel.channelPic}`,
-                keyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID!,
-                privateKey: custom,
-                dateLessThan: `${new Date(Date.now() + 60 * 60 * 24)}`,
-              });
-            }
-          }
-      /* <------------------> */
-      
-      }
+
     return { success: true, code: 200, data:comments };
   } catch (e: any) {
     logg.fatal(e.message);

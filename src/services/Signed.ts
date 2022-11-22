7f74eba5082888178c69642b0374939edae28219
JBB3-LBH <jbbrown@hypercho.com>
2022-11-22 01:04:19 +0000

    signed url conditions

--- src/services/Signed.ts
+++ src/services/Signed.ts
@@ -23,7 +23,7 @@ export const signedUrl_forOne = async (s3Path: string): Promise<string> => {
 
     return newUrl;
   } catch (e: any) {
-    logg.warn(e.message)
+    logg.warn(e.message);
     throw new Error(e.message);
   }
 };
@@ -33,51 +33,50 @@ export const signedUrl_forOne = async (s3Path: string): Promise<string> => {
 export const signedUrl_forOneScene = async ({ videoId, _id }: TypesforScene): Promise<oneSceneTypes> => {
   //sign with cloudfront
 
- try {
-  if (videoId.includes("http")) {
-    return { videoId: "", _id };
-  }
-  const newUrl = getSignedUrl({
-    url: `https://d27i2oedcihbcx.cloudfront.net/${videoId}`,
-    keyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID!,
-    privateKey: custom!,
-    dateLessThan: `${new Date(Date.now() + 60 * 60 * 24)}`,
-  });
+  try {
+    if (videoId.includes("http")) {
+      return { videoId: "", _id };
+    }
+    const newUrl = getSignedUrl({
+      url: `https://d27i2oedcihbcx.cloudfront.net/${videoId}`,
+      keyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID!,
+      privateKey: custom!,
+      dateLessThan: `${new Date(Date.now() + 60 * 60 * 24)}`,
+    });
 
-  return { videoId: newUrl, _id };
+    return { videoId: newUrl, _id };
   } catch (e: any) {
-    logg.warn(e.message)
+    logg.warn(e.message);
     throw new Error(e.message);
   }
 };
 
 //signed url for array of scenes
 export const signedUrl_forScenes = async (ScenesList: TypesforScene[]): Promise<oneSceneTypes[]> => {
- try {
-  const finalArr = [];
-  for (let i = 0; i < ScenesList.length; i++) {
-    const { videoId, _id } = ScenesList[i];
-    if (!videoId || !_id) {
-        if(_id){
-            finalArr.push({ videoId: "", _id });
+  try {
+    const finalArr = [];
+    for (let i = 0; i < ScenesList.length; i++) {
+      const { videoId, _id } = ScenesList[i];
+      if (!videoId || !_id) {
+        if (_id) {
+          finalArr.push({ videoId: "", _id });
         }
+      } else if (videoId.includes("http")) {
+        finalArr.push({ videoId: "", _id });
+      } else {
+        //sign with cloudfront
+        const newUrl = getSignedUrl({
+          url: `https://d27i2oedcihbcx.cloudfront.net/${videoId}`,
+          keyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID!,
+          privateKey: custom!,
+          dateLessThan: `${new Date(Date.now() + 60 * 60 * 24)}`,
+        });
+        finalArr.push({ videoId: newUrl, _id });
       }
-    else if (videoId.includes("http")) {
-      finalArr.push({ videoId: "", _id });
-    } else {
-      //sign with cloudfront
-      const newUrl = getSignedUrl({
-        url: `https://d27i2oedcihbcx.cloudfront.net/${videoId}`,
-        keyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID!,
-        privateKey: custom!,
-        dateLessThan: `${new Date(Date.now() + 60 * 60 * 24)}`,
-      });
-      finalArr.push({ videoId: newUrl, _id });
     }
-  }
-  return finalArr;
+    return finalArr;
   } catch (e: any) {
-    logg.warn(e.message)
+    logg.warn(e.message);
     throw new Error(e.message);
   }
 };
